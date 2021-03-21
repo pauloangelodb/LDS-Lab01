46ff6555359ac4c58b8b1071c82af830932991de
Paulo Angelo <paulo.barbosa@sga.pucminas.br>
2021-03-21 19:23:43 -0300

    Implementação inicial de usuário, aluno e professor

---
+++ implementation/LDS Prototipo/src/Aluno.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Aluno extends Usuario {
+    
+    //Construtor da classe Aluno
+    public Aluno(int matricula, String senha, String nome, int origem) {
+        this.setMatricula(matricula);
+        this.setSenha(senha);
+        this.setNome(nome);
+        this.setOrigem(origem);
+    }
+
+    public void matricular(int idDisciplina) {
+
+        /* IMPLEMENTAR CHAMADA DA CLASSE CURSO */
+        curso.matricular(this.getMatricula(), idDisciplina);
+
+    }
+
+    public void cancelarMatricula(int idDisciplina) {
+
+        /* IMPLEMENTAR CHAMADA DA CLASSE CURSO */
+        curso.cancelarMatricula(this.getMatricula(), idDisciplina);
+    }
+}
